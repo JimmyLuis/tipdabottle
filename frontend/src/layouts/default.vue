cab45f433119c120b1831fd20394c5d8119dd78d
JimmyLuis <lbeckhoff.lb@gmai.com>
2025-03-28 15:38:42 +0100

    + New frontend structure + added first components

---
+++ frontend/src/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-main>
+    <NavigationBar />
+    <member-slot></member-slot>
+  </v-main>
+</template>
+
+<script setup>
+  //
+import NavigationBar from "@/components/NavigationBar.vue";
+  import MemberSlot from "@/components/MemberSlot.vue";
+</script>
